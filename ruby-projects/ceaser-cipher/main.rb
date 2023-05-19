655ecad95d6cf01706682d2dd1364477adf2636f
Pedro Rocha <pedrorochadejesus.13@gmail.com>
2023-05-19 12:00:45 -0300

    Cipher concluido

--- ruby-projects/ceaser-cipher/main.rb
+++ ruby-projects/ceaser-cipher/main.rb
@@ -1,14 +1,22 @@
 
 print "Hurry! write the message: "
-x = gets
+x = gets.chomp
 print "And now tell a number for encryption key: "
 y = gets.to_i
 
 def ceasar_cipher(message, key)
+    encrypt = ""
     ascii = message.chars.map {|c| c.ord}
-    changed = ascii.map {|c| c + key}
-    changed.map! {|c| c.chr}
-    puts changed.join
+    ascii.each do |c|
+        first = c < 91 ? 65 : 97
+        if c.between?(65,90) || c.between?(97,122)
+            shift = (((c - first) + key) % 26) + first
+            encrypt += shift.chr 
+        else 
+            encrypt += c.chr
+        end
+    end
+    puts encrypt
 end
 
 ceasar_cipher(x,y)
