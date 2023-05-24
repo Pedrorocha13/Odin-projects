527a962de0fe7b31612a3f0c2356fe9d4d7aa31f
Pedro Rocha <pedrorochadejesus.13@gmail.com>
2023-05-23 22:15:19 -0300

    Incluindo alterações

--- ruby-projects/stock-picker/main.rb
+++ ruby-projects/stock-picker/main.rb
@@ -1,15 +1,20 @@
 def stock(stocks)
-	buy = []
 
-	stocks.map do |buy_stock|
-		profit = []
-		buy << profit
+  sell_price = 0
+  profit = 0
+  buy_price = 0
+  min_price = stocks.first
 
-		stocks.map do |sell_stock|
-			profit << sell_stock - buy_stock
-		end
-	end
-
+  stocks.each_with_index do |element,i|
+    if min_price > element
+      min_price = element
+      p "------------------"
+    elsif sell_price < (element - min_price)
+      profit = element - min_price
+      sell_price = element
+      buy_price = stocks.index(min_price)
+    end
+  end
+  puts buy_price,sell_price
 end
-
-stock([17,3,6,9,15,8,6,1,10])
+stock([17,3,6,9,15,8,6,1,15])
